9a9da130b55fbb28332b98b1ab24ad2870a37883
Mark Daniel Kintu <danielmark973@gmail.com>
2024-02-16 16:34:08 +0300

    added a login form and modified the login html

--- sword/accounts/forms.py
+++ sword/accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 User = get_user_model()  # Get the current user model (CustomUser)
 
@@ -10,3 +12,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Username', max_length=30,
+                               widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(request=self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError("Invalid username or password")
+        return self.cleaned_data
--- sword/accounts/views.py
+++ sword/accounts/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST, request=request)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 # Create your views here.
 def signup(request):
@@ -16,17 +27,28 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
+# def login_view(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('index')  # Replace 'home' with appropriate redirect 
+#         else:
+#             messages.error(request, 'Username or password is incorrect.')
+#     return render(request, 'accounts/login.html')
+
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = LoginForm(data=request.POST, request=request)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            return redirect('index')  # Replace 'home' with appropriate redirect 
-        else:
-            messages.error(request, 'Invalid username or password.')
-    return render(request, 'accounts/login.html')
+            return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
